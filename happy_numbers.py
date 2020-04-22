e092f75be547a77bd0a756fc7c1ce63fcfb7d7c6
MichalSl <45119362+MichalSl@users.noreply.github.com>
2020-04-22 22:32:53 +0800

    Update happy_numbers.py

--- happy_numbers.py
+++ happy_numbers.py
@@ -2,35 +2,59 @@ def HappyNumbers():
     
     print("Welcome to the happy numbers checker!")
     
-    original_number = int(input("Enter a number: "))    # the number input
-    number = original_number    # copy of the original number to operate with
-    new_number = 0 
-    counter = 0    # counter to count steps
-    results = []    #list of results of the operations
-    
-    if number == 1:
-        print("Number {} is a happy number".format(original_number))
-        
-    
-    
-    else:
-        while counter < 20:    # setting counter to 20 tries
-            number_str = str(number)    # creating a string of the number
-            new_number_list = [int(x)**2 for x in number_str]    # creating a list of squared digits of the number
-            new_number = sum(new_number_list)    # sum of the squared digits
+    while True:
+        try:
+            original_number = int(input("Enter a number: "))    # the number input
             
-            if new_number == 1:    # if the sum of squared digits (new_number) = 1 - original number is happy
-                print("Number {} is a happy number".format(original_number))
-                counter += 1
-                print(results)
-                break
-                
-            else:    # if the sum of squared digits (new_number) != 1 try again and increase the counter
-                counter += 1
-                results.append(new_number)
-                number = new_number
-                continue
+        except ValueError:
+            print("Enter a number: ")
+            
+        else:
+            number = original_number    # copy of the original number to operate with
+            new_number = 0 
+            counter = 0    # counter to count steps
+            results = []    #list of results of the operations
     
-        else:    # if after 20 tries the sum of the squared digits != 1 ---> the original number is not happy 
-            print("Number {} is not happy number".format(original_number))
+            if number == 1:
+                print("Number {} is a happy number".format(original_number))
+
+
+
+            else:
+                while counter < 20:    # setting counter to 20 tries
+                    number_str = str(number)    # creating a string of the number
+                    new_number_list = [int(x)**2 for x in number_str]    # creating a list of squared digits of the number
+                    new_number = sum(new_number_list)    # sum of the squared digits
+
+                    if new_number == 1:    # if the sum of squared digits (new_number) = 1 - original number is happy
+                        print("Number {} is a happy number".format(original_number))
+                        counter += 1
+                        print(results)
+                        
+                        reply = input("Do you want to try again? Y / N ")
+                    
+                        if reply.lower().startswith("y"):
+                            continue
+                        else:
+                            print("Bye!")
+                            break
+
+                    else:    # if the sum of squared digits (new_number) != 1 try again and increase the counter
+                        counter += 1
+                        results.append(new_number)
+                        number = new_number
+                        continue
+
+                else:    # if after 20 tries the sum of the squared digits != 1 ---> the original number is not happy 
+                    print("Number {} is not happy number".format(original_number))
+                    
+                    reply = input("Do you want to try again? Y / N ")
+                    
+                    if reply.lower().startswith("y"):
+                        continue
+                    else:
+                        print("Bye!")
+                        break
+                    
+                    
             
